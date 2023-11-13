65347453eb459d5e554a597fc4516bb25297ca31
Guillermo Alvarez <alvarezf.guillermo@gmail.com>
2023-11-13 19:39:26 -0300

    Primer commit

---
+++ tags/src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import InputTag from "./components/inputTag.vue"
+
+export default {
+    components:{
+        InputTag,
+    },
+    methods: {
+        handleOnTagsChange(tags){
+            console.log(tags);
+        },
+    },
+};
+
+</script>
+
+<template><InputTag @onTagsChange="handleOnTagsChange"/></template>
+
+<style scoped>
+
+</style>
